621ecc2713bb1b07e114d2f65714ccf1d6707683
Kevin Fan <kevinlifan@yahoo.com>
2023-05-09 00:52:54 -0400

    minor styling changes

--- client/pages/mainPage.jsx
+++ client/pages/mainPage.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import MainContainer from '../containers/mainContainer.jsx';
-import PublicContainer from '../containers/publicContainer.jsx'
+import PublicContainer from '../containers/publicContainer.jsx';
 import { useSelector } from 'react-redux';
 
 export default function mainPage() {
   const loggedIn = useSelector((state) => state.reducer.loggedIn);
 
   return loggedIn ? (
-    <div>
+    <div className='main-div'>
       <MainContainer />
     </div>
   ) : (
-    <div>
+    <div className='main-div'>
       <PublicContainer />
     </div>
   );
