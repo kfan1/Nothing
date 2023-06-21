29e5f78e92cd8c27dc13574fa63a82e6e578e607
Kevin Fan <kevinlifan@yahoo.com>
2023-06-21 10:12:05 -0400

    deleted unessary files

--- server/models/queryModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const querySchema = new Schema({
-  username: { type: String, required: true },
-  query: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Query', querySchema);
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from './reducers/reducer'
-
-export const store = configureStore({
-  reducer: {
-    reducer: reducer,
-  },
-});
